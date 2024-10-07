3e0840c3eb8e378e69e088342fcd977e02660961
kamranwani <kamraanwani@gmail.com>
2024-10-07 16:58:34 +0530

    fixed wordcloud

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import axios from "axios";
 
 function App() {
   const [clusters, setClusters] = useState([]);
+  const [wordclouds, setWordclouds] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [pdfFile, setPdfFile] = useState(null);
   const [fileName, setFileName] = useState("");
-  const [nClusters, setNClusters] = useState(5); // State to track the file name
+  const [nClusters, setNClusters] = useState(5);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setPdfFile(file);
-    setFileName(file ? file.name : ""); // Update fileName state
+    setFileName(file ? file.name : "");
   };
 
   const handleSubmit = async (e) => {
@@ -35,16 +36,20 @@ function App() {
         }
       );
 
+      console.log(response.data);
       setClusters(response.data.clusters);
+      setWordclouds(response.data.wordclouds);
     } catch (err) {
       setError(err.message);
     } finally {
       setLoading(false);
     }
   };
+
   useEffect(() => {
     console.log(clusters);
   }, [clusters]);
+
   return (
     <div className="min-h-screen custom-gradient px-12 pb-16 pt-4 flex flex-col gap-8">
       <div>
@@ -74,7 +79,7 @@ function App() {
             )}
           </div>
           <div>
-            <div className="mb-4 flex  items-center gap-2">
+            <div className="mb-4 flex items-center gap-2">
               <label className="block mb-1 text-xs text-neutral-300">
                 Number of Clusters:
               </label>
@@ -90,7 +95,7 @@ function App() {
             <div className="flex justify-end">
               <button
                 type="submit"
-                className="mt-4 hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-500 bg-gradient-to-r from-blue-800 to-green-700 text-white p-2 rounded text-xs px-5"
+                className="mt-4 hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-500 bg-gradient-to-r from-blue-800 to-green-700 text-neutral-900 p-2 rounded text-xs px-5"
               >
                 Upload PDF
               </button>
@@ -108,7 +113,7 @@ function App() {
         {clusters.map((cluster, index) => (
           <div
             key={index}
-            className="bg-black/10 hover:bg-black/40 backdrop-blur-lg rounded-lg shadow-lg p-4 gap-3 flex flex-col items-start  max-h-[300px] overflow-auto specific-component group"
+            className="bg-black/10 hover:bg-black/40 backdrop-blur-lg rounded-lg shadow-lg p-4 gap-3 flex flex-col items-start max-h-[300px] overflow-auto specific-component group"
           >
             <h2 className="font-semibold text-lg text-neutral-100 group-hover:text-blue-500">
               Cluster {cluster.cluster}
@@ -139,11 +144,15 @@ function App() {
               })}
             </ul>
 
-            {cluster.wordcloud && (
+            {wordclouds[`cluster_${cluster.cluster}_wordcloud`] && (
               <div className="mt-4">
-                <h3 className="font-medium">Word Cloud:</h3>
+                <h3 className="font-medium text-neutral-100 text-lg">
+                  Word Cloud:
+                </h3>
                 <img
-                  src={`data:image/png;base64,${cluster.wordcloud}`}
+                  src={`data:image/png;base64,${
+                    wordclouds[`cluster_${cluster.cluster}_wordcloud`]
+                  }`}
                   alt={`Cluster ${cluster.cluster} Word Cloud`}
                   className="mt-2"
                 />
